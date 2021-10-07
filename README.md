4d7fa89a008eba586171fbb47a3cbb1ffc0a404a
Jagjeevan Nahal <>
2021-10-07 03:30:41 +0000

    Changed late_strings to a tuple.

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-Anti-Late Discord Bot
-=====================
+Requirements 
 
-# Todo
-- [ ] Requirements
-
-# Requirements
-- [ ] it should be able to set a time 
-- [ ] it should keep score of individual users
-- [ ] it should score points based on set rules 
-- [ ] it should   
+  - [ ] It should store a date and time 
+  - [ ] It should use year-month-day format
+  - [ ] It should award points based on time of arrival
+  - [ ] It should keep a tally of points for indivdual users
+  - [ ] It should have admin permissions
+  - [ ] It should ping everyone when time is up
+  - [ ] It should have a simple command to set time of event 
+  - [ ] It should message in only one channel 
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 # https://github.com/Rapptz/discord.py
 import discord
 import os
-import time
-from discord.ext import commands
-from nltk.sentiment import SentimentIntensityAnalyzer
 
 client = discord.Client()
-sia = SentimentIntensityAnalyzer()
 
-async def punish_sad_people(msg):
-    sentiment = sia.polarity_scores(msg.content)
-    print(sentiment)
-    
-    if sentiment['compound'] < 0:
-        await msg.channel.send('https://c.tenor.com/IweIMqGS1gkAAAAC/questioning-are-you-sad.gif')
-    
 
 @client.event
 async def on_ready():
@@ -22,23 +11,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    late_strings = ["I AM LATE", "IM LATE", "I'M LATE"]
+    late_strings = ("I AM LATE", "IM LATE", "I'M LATE")
     if message.author == client.user:
         return
     uppercase_input = message.content.upper()
-    await punish_sad_people(message)
 
     for late_string in late_strings:
         if late_string in uppercase_input:
             await message.channel.send('https://tenor.com/baxTB.gif')
+            
 
 
-
-# bot = commands.Bot(command_prefix='>')
-
-# @bot.command()
-# async def here(ctx):
-#     await ctx.send(time.time())
-
-# bot.run(os.environ['DISCORD_TOKEN'])
 client.run(os.environ['DISCORD_TOKEN'])
